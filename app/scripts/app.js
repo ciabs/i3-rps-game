9bc68f19d2fce04594c9de1a846120906478254f
Francesco Fiori <francesco.fiori@gmail.com>
2016-03-15 19:33:42 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name i3RpsGameApp
+ * @description
+ * # i3RpsGameApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('i3RpsGameApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name i3RpsGameApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the i3RpsGameApp
+ */
+angular.module('i3RpsGameApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
